ecfd9fd7c09eecb6da13df09497e6dae97854f4a
Unknown <vm@tsl.io>
2018-01-20 11:27:21 -0500

    Utilities

---
+++ CryptoQuest/Extensions/Utilities.swift
@@ -0,0 +1,222 @@
+//
+//  SCNVector3+Extension.swift
+//  CryptoQuest
+//
+//  Created by Vasilii Muravev on 1/20/18.
+//  Copyright © 2018 SilverLogic, LLC. All rights reserved.
+//
+
+import ARKit
+import SceneKit
+import UIKit
+
+// MARK: - SCNVector3 Extension
+enum PointType {
+    case horizontal
+    case vertical
+}
+
+extension SCNVector3 {
+    func length() -> Float {
+        return sqrtf(x * x + y * y + z * z)
+    }
+    
+    func distanceFromVector(_ vector: SCNVector3) -> Float {
+        return (self - vector).length()
+    }
+    
+    func flatPoint(_ type: PointType = .horizontal) -> CGPoint {
+        switch type {
+        case .horizontal:
+            return CGPoint(x: CGFloat(x), y: CGFloat(z))
+        case .vertical:
+            return CGPoint(x: CGFloat(x), y: CGFloat(y))
+        }
+    }
+}
+
+
+// MARK: - Operator Overloads
+func - (lhs: SCNVector3, rhs: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z)
+}
+
+func + (lhs: SCNVector3, rhs: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
+}
+
+func * (lhs: SCNVector3, rhs: Float) -> SCNVector3 {
+    return SCNVector3Make(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs)
+}
+
+func == (lhs: SCNVector3, rhs: SCNVector3) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z
+}
+
+func != (lhs: SCNVector3, rhs: SCNVector3) -> Bool {
+    return !(lhs == rhs)
+}
+
+
+// MARK: - CGPoint Extension
+extension CGPoint {
+    init(_ vector: SCNVector3) {
+        x = CGFloat(vector.x)
+        y = CGFloat(vector.y)
+    }
+    
+    func normalized(for size: CGSize) -> CGPoint {
+        if size.width == 0 || size.height == 0 {
+            return CGPoint.zero
+        }
+        return CGPoint(x: x / size.width, y: y / size.height)
+    }
+    
+    func length() -> CGFloat {
+        return sqrt(x * x + y * y)
+    }
+    
+    func angle() -> CGFloat {
+        guard x != 0 else { return 0.0 }
+        let angle = atan(y / x)
+        return CGFloat.pi * (x > 0 ? 0.5 : 1.5) - angle
+    }
+}
+
+
+// MARK: - Operator Overloads
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+func / (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x / right, y: left.y / right)
+}
+
+func * (left: CGPoint, right: CGFloat) -> CGPoint {
+    return CGPoint(x: left.x * right, y: left.y * right)
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func /= (left: inout CGPoint, right: CGFloat) {
+    left = left / right
+}
+
+func *= (left: inout CGPoint, right: CGFloat) {
+    left = left * right
+}
+
+
+// MARK: - SCNNode
+extension SCNNode {
+    func look(at pointOfView: SCNNode, offset: SCNVector3?) {
+        let constraint = SCNLookAtConstraint(target: pointOfView)
+        constraint.isGimbalLockEnabled = true
+        if let offset = offset {
+            constraint.targetOffset = offset
+        }
+        constraints = [constraint]
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak constraint] in
+            constraint?.influenceFactor = 0.03
+        }
+    }
+    
+    func parentOfType<T: SCNNode>() -> T? {
+        var node: SCNNode! = self
+        repeat {
+            if let node = node as? T { return node }
+            node = node?.parent
+        } while node != nil
+        return nil
+    }
+    
+    func performAction(_ independent: Bool = false) {
+        var node: SCNNode! = self
+        repeat {
+            if let actionNode = node as? ActionNodeProtocol,
+                let action = actionNode.action {
+                action(node)
+                if !independent { break }
+            }
+            node = node?.parent
+        } while node != nil
+    }
+}
+
+
+// MARK: - UIColor
+extension UIColor {
+    static func colorFromHexValue(_ hexValue: UInt, alpha: CGFloat = 1.0) -> UIColor {
+        let redValue = ((CGFloat)((hexValue & 0xFF0000) >> 16)) / 255.0
+        let greenValue = ((CGFloat)((hexValue & 0xFF00) >> 8)) / 255.0
+        let blueValue = ((CGFloat)(hexValue & 0xFF)) / 255.0
+        return UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: alpha)
+    }
+}
+
+
+// MARK: - SCNMaterial
+extension SCNMaterial {
+    
+    // MARK: - Initializers
+    convenience init(color: UIColor) {
+        self.init()
+        diffuse.contents = color
+    }
+    
+    convenience init(image: UIImage) {
+        self.init()
+        diffuse.contents = image
+    }
+}
+
+
+// MARK: - matrix_float4x4
+extension matrix_float4x4 {
+    func position() -> SCNVector3 {
+        return SCNVector3(columns.3.x, columns.3.y, columns.3.z)
+    }
+}
+
+
+// MARK: - ARFrame
+extension ARFrame {
+    func featurePoint(for normalizedPoint: CGPoint) -> matrix_float4x4? {
+        let x = normalizedPoint.x
+        let y = normalizedPoint.y
+        if x < 0.0 || x > 1.0 || y < 0.0 || y > 1.0 {
+            return nil
+        }
+        guard let testResult = hitTest(normalizedPoint, types: .featurePoint).first else {
+            return nil
+        }
+        return testResult.worldTransform
+    }
+    
+    func existingPlanePoint(for normalizedPoint: CGPoint) -> matrix_float4x4? {
+        let x = normalizedPoint.x
+        let y = normalizedPoint.y
+        if x < 0.0 || x > 1.0 || y < 0.0 || y > 1.0 {
+            return nil
+        }
+        guard let testResult = hitTest(normalizedPoint, types: [.existingPlaneUsingExtent]).first else {
+            return nil
+        }
+        return testResult.worldTransform
+    }
+}
