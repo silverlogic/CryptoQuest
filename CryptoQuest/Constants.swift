9274f0a24d9d6646692a6d1cf99068a6faf98a8d
Emanuel Guerrero <emanuelguerrerodev@gmail.com>
2018-01-20 11:52:36 -0500

    Added map view for discovering crypto

---
+++ CryptoQuest/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  CryptoQuest
+//
+//  Created by Emanuel  Guerrero on 1/20/18.
+//  Copyright © 2018 SilverLogic, LLC. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Networking Constants
+private let baseHost = ""
+let BASE_URL = "\(baseHost)/api/v1"
+
+
+// MARK: - Google API Keys
+let GOOGLE_MAPS_API_KEY = "AIzaSyAU1PiQ6gWSYt27S_xEb_wyznAGmpmpaX8"
