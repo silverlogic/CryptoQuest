90afe4abc3695a639cdef7d1563ce5085b8eb1db
Emanuel Guerrero <emanuelguerrerodev@gmail.com>
2018-01-21 02:00:07 -0500

    Added User model

---
+++ CryptoQuest/Models/EntityModels/User.swift
@@ -0,0 +1,49 @@
+//
+//  User.swift
+//  CryptoQuest
+//
+//  Created by Emanuel  Guerrero on 1/21/18.
+//  Copyright © 2018 SilverLogic, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    // MARK: - Public Instance Attributes
+    var userId: Int
+    var bitcoinAmount: Float
+    var bitcoinCashAmount: Float
+    var veztAmount: Float
+    var ethereumAmount: Float
+    var litecoinAmount: Float
+    var neoAmount: Float
+}
+
+
+// MARK: - Public Class Methods
+extension User {
+    static func mockUser1() -> User {
+        return User(
+            userId: 1,
+            bitcoinAmount: 0.0003223,
+            bitcoinCashAmount: 0.00210707,
+            veztAmount: 0.0,
+            ethereumAmount: 0.00358395,
+            litecoinAmount: 0.01985001,
+            neoAmount: 0.0
+        )
+    }
+    
+    static func mockUser2() -> User {
+        return User(
+            userId: 2,
+            bitcoinAmount: 0.0,
+            bitcoinCashAmount: 0.0,
+            veztAmount: 0.0,
+            ethereumAmount: 0.0,
+            litecoinAmount: 0.0,
+            neoAmount: 0.0
+        )
+    }
+}
